9ae83944e0094032e2afa3eedc9522a192bae5d5
Andrew Dyachenko <north.inhale@gmail.com>
2018-09-15 00:18:55 +0300

    Loading...

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Helmet from 'react-helmet'
+import cssVars from 'css-vars-ponyfill';
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Helmet>
+					<html lang="ru" />
+					<title>Нямушка</title>
+				</Helmet>
+				<main className="main">
+					<h1>Ты сегодня покормил кота?</h1>
+				</main>
+			</div>
+		)
+	}
+	componentDidMount() {
+		this.cssVars = cssVars();
+	}
+	componentWillUnmount() {
+		delete this.cssVars;
+	}
 }
 
 export default App;
